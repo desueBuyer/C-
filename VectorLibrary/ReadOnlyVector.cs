0bed0c8c53487b3d542aa4ccdb15c8360e2b1445
Alim Lyukmanov <50545653+desueBuyer@users.noreply.github.com>
2019-11-20 18:50:35 +0300

    VectorLibrary

---
+++ VectorLibrary/ReadOnlyVector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadOnlyVectorTask
+{
+    public class ReadOnlyVector
+    {
+        public readonly double X;
+        public readonly double Y;
+
+        public ReadOnlyVector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public ReadOnlyVector Add(ReadOnlyVector other) => new ReadOnlyVector(other.X + this.X, other.Y + this.Y);
+
+        public ReadOnlyVector WithX(double newX) => new ReadOnlyVector(newX, this.Y);
+
+        public ReadOnlyVector WithY(double newY) => new ReadOnlyVector(this.X, newY);
+    }
+}
---
+++ VectorLibrary/Vector.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace GeometryTasks
+{
+    public class Vector
+    {
+        public double X;
+        public double Y;
+        public Vector Add(Vector v1, Vector v2)
+        {
+            return new Vector { X = v1.X + v2.X, Y = v1.Y + v2.Y };
+        }
+    }
+
+    public class Segment
+    {
+        public Vector Begin = new Vector();
+        public Vector End = new Vector();
+    }
+
+    public class Geometry
+    {
+        public static double GetLength(Vector vector)
+        {
+            return Math.Sqrt(vector.X * vector.X + vector.Y * vector.Y);
+        }
+
+        public static double GetLength(Segment sgm)
+        {
+            double xLength = Math.Abs(sgm.Begin.X - sgm.End.X);
+            double yLength = Math.Abs(sgm.Begin.Y - sgm.End.Y);
+            return GetLength(new Vector { X = xLength, Y = yLength });
+        }
+
+        public static bool IsVectorInSegment(Vector vector, Segment segment)
+        {
+            double dotX = vector.X;
+            double dotY = vector.Y;
+            double sBeginX = segment.Begin.X;
+            double sBeginY = segment.Begin.Y;
+            double sEndX = segment.End.X;
+            double sEndY = segment.End.Y;
+            if (dotX >= sBeginX && dotX <= sEndX && dotY >= sBeginY && dotY <= sEndY)
+            {
+                double dx1 = sEndX - sBeginX;
+                double dy1 = sEndY - sBeginY;
+                double dx = dotX - sBeginX;
+                double dy = dotY - sBeginY;
+                double s = dx1 * dy - dx * dy1;
+                if (s == 0)
+                    return true;
+                return false;
+            }
+            else return false;
+        }
+
+
+        public static Vector Add(Vector v1, Vector v2)
+        {
+            return new Vector { X = v1.X + v2.X, Y = v1.Y + v2.Y };
+        }
+    }
+}
