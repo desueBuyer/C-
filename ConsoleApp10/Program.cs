bb4b133400656ec72c693ce61412aa837b17eeb3
Alim Lyukmanov <50545653+desueBuyer@users.noreply.github.com>
2019-11-20 19:05:59 +0300

    Goat with Rope

---
+++ ConsoleApp10/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int squareLenth = int.Parse(Console.ReadLine());
+            int ropeLenth = int.Parse(Console.ReadLine());
+            if (squareLenth<ropeLenth/2)
+            {
+                Console.WriteLine("Весь огород будет съеден!");
+            }
+            else
+            {
+                double eatenArea = Math.Round(Math.Pow(ropeLenth, 2) * Math.PI / Math.Pow(squareLenth, 2), 2); 
+                Console.WriteLine("Будет съедена {0} часть огорода!", eatenArea);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
