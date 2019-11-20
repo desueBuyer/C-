7f3ce6887435953a1f5e1ff548f7ae54942ae495
Alim Lyukmanov <50545653+desueBuyer@users.noreply.github.com>
2019-11-20 19:01:04 +0300

    SummOfNumbersInBook

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int str = 1;
+            int sum = 0;
+            while (sum <1095)
+            {
+                if (str<10)
+                {
+                    sum++;
+                }
+                else
+                {
+                    if (str>=10&&str<100)
+                    {
+                        sum += 2;
+                    }
+                    else
+                    {
+                        if (str>=100&&str<1000)
+                        {
+                            sum += 3;
+                        }
+                    }
+                }
+                str++;
+            }
+            Console.WriteLine(str);
+            Console.ReadKey();
+        }
+    }
+}
